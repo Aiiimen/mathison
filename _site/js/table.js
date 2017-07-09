8ffc3b7db1dfeb1cec664e9ef1728afcc0ec5f70
AimenBenAissa <aimenbenfika@gmail.com>
2017-07-09 20:23:35 +0100

    mathison

---
+++ _site/js/table.js
@@ -0,0 +1,6 @@
+$(function(){
+
+  //table sorting plugin
+
+  $('#example').DataTable();
+});
--- js/functions.js
+++ js/functions.js
@@ -1,59 +1,42 @@
-$(document).ready(function(){
+$(function(){
+
+   // jQuery methods
 
 
-  //nav bar on click
-  $('.cato_link:first').css("color", "#110f0f");
 
-  $('.cato_link').click(function(e){
-
-    $('.cato_link').css("color","#bfbfbf");
-    $(this).css("color","#110f0f");
-
-  });
-
-  //photo belt
-
-  $('.cato_link:first').click(function(){
-
-    $('.photos_belt').animate({
-
-      left: "0%"},500);
-  });
-
-  $('.cato_link:nth-child(2)').click(function(){
-
-    $('.photos_belt').animate({
-      left: "-100%"},500);
-  });
+   //AOS animation
+   animationsOnScroll();
+   //smooth scroll
+   smoothScroll();
 
 
-  //thumbs on click
-  var h = $('.photo_holder').height();
-  var w = $('.photo_holder').width();
-
-  var photoContainer = $('.photos_containers').width();
-
-  console.log(h);
-  console.log(w);
-  console.log(photoContainer);
-
-//expand
-  $('.thumbs img:nth-child(2)').click(function(){
-
-    $('.photo_holder').animate({
-      height: photoContainer * 60 / 100,
-      width: photoContainer
-    },800);
-  });
-
-//shrink
-  $('.thumbs img:nth-child(1)').click(function(){
-    console.log("hello");
-    $('.photo_holder').animate({
-      height: h,
-      width: w
-    },800);
-  });
 
 
 });
+
+
+function dataTable(){
+    $('#example').DataTable();
+}
+
+
+function animationsOnScroll(){
+  AOS.init({
+    duration: 800,
+  });
+}
+
+
+function smoothScroll(){
+  //smooth scrolling method
+
+  $('a[href^="#"]').on('click', function(event){
+    var target = $( $(this).attr('href'));
+    if(target.length){
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      },1000)
+    }
+  });
+}
